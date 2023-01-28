b65e5ea5b5bde645480eab27509f6244f33cd81d
AlexLongbeard <AGLBRDK.KR@gmail.com>
2023-01-28 17:05:23 +0600

    done_1

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -3,6 +3,6 @@ package school.mjc.stage0.conditions.finalTask;
 public class BitwiseValuesSwap {
     public void swap(int first, int second) {
         first=first^second^(second=first);
-        System.out.println(first,second);
+        System.out.println(""+first + ""+second);
     }
 }
