d35833554c81a5add3ffa9fc34a6a08d13095c86
AlexLongbeard <AGLBRDK.KR@gmail.com>
2023-01-28 17:15:27 +0600

    done_3

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.conditions.finalTask;
 public class DaysInMonth {
     public void printDays(int year, int month) {
         byte leapYear=2;
-        if (year<=0) {
+        if (year<=0 && (month<1 || month>12)) {
             System.out.println("invalid date");
         }
         else if (year%4!=0) {
@@ -28,7 +28,6 @@ public class DaysInMonth {
             switch (month) {
                 case (1), (3), (5), (7), (8), (10), (12) -> System.out.println("31");
                 case (4), (6), (9), (11) -> System.out.println("30");
-                default -> System.out.println("invalid date");
             }
             }
     }
